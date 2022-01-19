d93560d035217ca2291e336a3ff635a15cd287b3
javiro93 <jotarodriguez939@gmail.com>
2022-01-19 18:37:19 +0100

    add lightbox

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
+import React, { useState } from 'react'
 import './App.css'
 import Typography from '@mui/material/Typography'
-
+import CallIcon from '@mui/icons-material/Call'
+import Paper from '@mui/material/Paper'
+import BottomNavigation from '@mui/material/BottomNavigation'
+import Button from '@mui/material/Button'
 import Carousel from './components/carousel/Carousel'
+import LightBox from './components/lightbox/LightBox '
 
 function App() {
-  const captionStyle = {
-    fontSize: '2em',
-    fontWeight: 'bold',
+  const [toggle, setToggle] = useState(false)
+  const [sIndex, setSIndex] = useState(0)
+
+  const lightBoxHandler = (state, sIndexAux) => {
+    setToggle(state)
+    setSIndex(sIndexAux)
   }
-  const slideNumberStyle = {
-    fontSize: '20px',
-    fontWeight: 'bold',
-  }
+  const data = [
+    {
+      image: 'https://a.ccdn.es/cnet/2021/12/18/50840372/528052845_g.jpg',
+      caption: `<div>
+                  San Francisco
+                  <br/>
+                  Next line
+                </div>`,
+    },
+    {
+      image: 'https://a.ccdn.es/cnet/2021/12/18/50840372/528052847_g.jpg',
+      caption: 'Scotland',
+    },
+    {
+      image: 'https://a.ccdn.es/cnet/2021/12/18/50840372/528052849_g.jpg',
+      caption: 'Darjeeling',
+    },
+    {
+      image: 'https://a.ccdn.es/cnet/2021/12/18/50840372/528052853_g.jpg',
+      caption: 'San Francisco',
+    },
+    {
+      image: 'https://a.ccdn.es/cnet/2021/12/18/50840372/528052857_g.jpg',
+      caption: 'Scotland',
+    },
+    {
+      image: 'https://a.ccdn.es/cnet/2021/12/18/50840372/528052862_g.jpg',
+      caption: 'Darjeeling',
+    },
+    {
+      image: 'https://a.ccdn.es/cnet/2021/12/18/50840372/528052865_g.jpg',
+      caption: 'San Francisco',
+    },
+    {
+      image: 'https://a.ccdn.es/cnet/2021/12/18/50840372/528052869_g.jpg',
+      caption: 'Scotland',
+    },
+    {
+      image: 'https://a.ccdn.es/cnet/2021/12/18/50840372/528052872_g.jpg',
+      caption: 'Darjeeling',
+    },
+    {
+      image: 'https://a.ccdn.es/cnet/2021/12/18/50840372/528052879_g.jpg',
+      caption: 'Darjeeling',
+    },
+  ]
+
   return (
     <div className="App">
+      <LightBox
+        state={toggle}
+        event={lightBoxHandler}
+        data={data}
+        imageWidth="90vw"
+        imageHeight="auto"
+        setImageIndex={setSIndex}
+        imageIndex={sIndex}
+      />
       <Typography variant="h3" component="div" gutterBottom>
         SE VENDE
       </Typography>
+      <Typography variant="h5" component="div" gutterBottom>
+        Ford Focus TDCi Trend 1.6 110cv 2005
+      </Typography>
 
-      <div className="App">
+      <div>
         <div style={{ textAlign: 'center' }}>
           <div
             style={{
@@ -26,14 +89,11 @@ function App() {
             }}
           >
             <Carousel
-              onclickImage={() => console.log('dentro')}
+              data={data}
+              openLightBoxHandler={lightBoxHandler}
               width="850px"
               height="500px"
-              captionStyle={captionStyle}
               radius="10px"
-              slideNumber
-              slideNumberStyle={slideNumberStyle}
-              captionPosition="bottom"
               automatic={false}
               dots
               pauseIconColor="white"
@@ -46,12 +106,23 @@ function App() {
                 textAlign: 'center',
                 maxWidth: '850px',
                 maxHeight: '500px',
-                margin: '40px auto',
+                margin: '0.35em auto',
               }}
             />
           </div>
         </div>
       </div>
+      <Paper
+        square
+        sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
+        elevation={3}
+      >
+        <BottomNavigation>
+          <Button fullWidth variant="contained" endIcon={<CallIcon />}>
+            Contacto
+          </Button>
+        </BottomNavigation>
+      </Paper>
     </div>
   )
 }
---
+++ src/components/lightbox/LightBox .jsx
@@ -0,0 +1,53 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/prop-types */
+
+import React from 'react'
+import './style.css'
+
+import { ReactComponent as Close } from './close.svg'
+
+function LightBox({
+  state,
+  event,
+  data,
+
+  imageWidth = '50vw',
+
+  setImageIndex,
+  imageIndex,
+}) {
+  if (state) {
+    return (
+      <div className="reactLightboxContainer">
+        <div className="reactLightboxCloseButtonContainer">
+          <button
+            className="reactLightboxCloseButton"
+            onClick={() => {
+              event(false, imageIndex)
+              setImageIndex(0)
+            }}
+          >
+            <Close fill="#fff" />
+          </button>
+        </div>
+        <div className="reactLightboxArea">
+          <img
+            src={data[imageIndex].image}
+            alt={data.title}
+            style={{
+              height: 'auto',
+
+              maxHeight: '95vh',
+              maxWidth: imageWidth,
+            }}
+          />
+        </div>
+      </div>
+    )
+  }
+
+  return null
+}
+export default LightBox
